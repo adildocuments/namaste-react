4e016fbdef23fde2597e44a18bb2425253849f4a
Adil Ali <adil.chetu@gmail.com>
2024-02-20 20:07:53 +0530

    add episode 1

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+    <div id="parent">
+        <div id="child">
+            <h1>I'm a h1 tag</h1>
+            <h2>I'm a h2 tag</h2>
+        </div>
+        <div id="child2">
+            <h1>I'm a h1 tag</h1>
+            <h2>I'm a h2 tag</h2>
+        </div>
+    </div>
+*/
+
+
+const parent = React.createElement("div", {id:"parent"}, 
+    [
+        React.createElement("div", {id:"child"}, 
+            [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h2', {}, 'I am 2 tag')]
+        ),
+        React.createElement("div", {id:"child2"}, 
+            [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h2', {}, 'I am 2 tag')]
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+root.render(parent);
